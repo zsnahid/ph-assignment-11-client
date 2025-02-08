fc61f0a6b6c6663c70ee306630c348c467b91fde
zsnahid <zahidsadmannahid@gmail.com>
2025-02-08 22:35:46 +0600

    designed one extra section in home page

--- src/components/MySolidButton.jsx
+++ src/components/MySolidButton.jsx
@@ -3,7 +3,7 @@ export default function MySolidButton({ children }) {
   return (
     <span className="group relative inline-block text-sm font-medium text-white focus:outline-none w-full">
       <span className="absolute inset-0 border border-deep-orange-700 group-active:border-deep-orange-800 rounded-md"></span>
-      <span className="block border border-deep-orange-700 bg-deep-orange-700 px-12 py-3 transition-transform active:border-deep-orange-800 active:bg-deep-orange-800 group-hover:-translate-x-1 group-hover:-translate-y-1 rounded-md">
+      <span className="text-center block border border-deep-orange-700 bg-deep-orange-700 px-12 py-3 transition-transform active:border-deep-orange-800 active:bg-deep-orange-800 group-hover:-translate-x-1 group-hover:-translate-y-1 rounded-md">
         {children}
       </span>
     </span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import { Typography } from "@material-tailwind/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import HomePageQueryCard from "../components/HomePageQueryCard";
+import MySolidButton from "../components/MySolidButton";
 import Slider from "../components/Slider";
 
 export default function Home() {
@@ -19,13 +21,46 @@ export default function Home() {
   return (
     <>
       <Slider />
-      <div className="grid grid-cols-3 gap-10 my-10 px-10">
+      <div className="grid lg:grid-cols-3 gap-10 my-10 w-11/12 mx-auto">
         {queries.map((query) => (
           <HomePageQueryCard
             key={query._id}
             query={query}
           />
         ))}
+      </div>
+      <div className="my-10 w-11/12 mx-auto grid lg:grid-cols-2 gap-5 lg:gap-20 items-center">
+        <div className="flex justify-between items-center">
+          <div className="grid lg:grid-cols-2 gap-5 items-center">
+            <img src="/src/assets/icon-1.svg" />
+            <div>
+              <Typography variant="h4">1M+</Typography>
+              <Typography>Members</Typography>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-2 gap-5 items-center">
+            <img src="/src/assets/icon-2.svg" />
+            <div>
+              <Typography variant="h4">100+</Typography>
+              <Typography>Forums</Typography>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-2 gap-5 items-center">
+            <img src="/src/assets/icon-3.svg" />
+            <div>
+              <Typography variant="h4">110+</Typography>
+              <Typography>Countries</Typography>
+            </div>
+          </div>
+        </div>
+        <div>
+          <MySolidButton>Start a discussion</MySolidButton>
+        </div>
+      </div>
+
+
+      <div>
+        
       </div>
     </>
   );
